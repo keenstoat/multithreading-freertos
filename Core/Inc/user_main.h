18a53dfb0a62ec29b50d4e2cf99b842347d23771
keenstoat <keenstoat>
2022-05-02 22:26:36 -0600

    usb comms with virtual com port

---
+++ Core/Inc/user_main.h
@@ -0,0 +1,20 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __USER_MAIN_H
+#define __USER_MAIN_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+/* ---------------------------------------------------------------------------*/
+
+
+
+
+int user_main(void);
+
+/* ---------------------------------------------------------------------------*/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USER_MAIN_H */
---
+++ Core/Src/user_main.cpp
@@ -0,0 +1,31 @@
+#include "user_main.h"
+#include "main.h"
+#include "usbd_cdc_if.h"
+#include "string.h"
+
+
+
+uint8_t userRxData[64];
+char txData[64];
+
+int user_main(void){
+
+
+
+
+  while(1){
+
+    if(strlen((char *) userRxData) > 0) {
+      strcpy(txData, "echo: ");
+      strcat(txData, (char *) userRxData);
+      strcat(txData, (char *) '\n');
+  //    CDC_Transmit_FS((uint8_t*) txData, strlen(txData));
+      CDC_Transmit_FS((uint8_t*) txData, strlen(txData));
+      memset(txData, '\0', 64);
+      memset(userRxData, '\0', 64);
+    }
+
+    HAL_Delay(1);
+
+  }
+}
