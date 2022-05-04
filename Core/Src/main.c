bbbd9de521800f649306023fbffb66aec2c47afb
keenstoat <keenstoat>
2022-05-03 18:22:07 -0600

    before using c++ strings

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,8 +19,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "cmsis_os.h"
-#include "usb_device.h"
 #include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -43,13 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -57,7 +49,6 @@ const osThreadAttr_t defaultTask_attributes = {
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-void StartDefaultTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -74,76 +65,16 @@ void StartDefaultTask(void *argument);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
   user_main();
-  /* USER CODE END 2 */
 
-  /* Init scheduler */
-  osKernelInitialize();
 
-  /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
-  /* USER CODE END RTOS_MUTEX */
-
-  /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
-  /* USER CODE END RTOS_SEMAPHORES */
-
-  /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
-  /* USER CODE END RTOS_TIMERS */
-
-  /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
-  /* USER CODE END RTOS_QUEUES */
-
-  /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
-  /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
-  /* USER CODE END RTOS_THREADS */
-
-  /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
-  /* USER CODE END RTOS_EVENTS */
-
-  /* Start scheduler */
-  osKernelStart();
-
-  /* We should never get here as control is now taken by the scheduler */
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+  // user_main() should be the actual starting point
+  // this while should never be reached
+  while (1);
 }
 
 /**
@@ -214,18 +145,7 @@ static void MX_GPIO_Init(void)
   * @retval None
   */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* init code for USB_DEVICE */
-  MX_USB_DEVICE_Init();
-  /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END 5 */
-}
+
 
 /**
   * @brief  Period elapsed callback in non blocking mode
--- Core/Src/user_main.cpp
+++ Core/Src/user_main.cpp
@@ -1,31 +1,68 @@
 #include "user_main.h"
 #include "main.h"
-#include "usbd_cdc_if.h"
-#include "string.h"
 
-//#include "FreeRTOS.h"
-//#include "task.h"
-//#include "semphr.h"
-//#include "stdio.h"
+#include "stdio.h"
+#include "string.h"
+#include <string>
+
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+
+
+
+void mainTask(void * arg);
+
 
 uint8_t userRxData[64];
 char txData[64];
 
 int user_main(void){
 
-  while(1){
+
+  xTaskCreate(
+      mainTask,
+      "mainTask",
+      configMINIMAL_STACK_SIZE,
+      ( void * ) NULL,
+      0,
+      NULL
+  );
+
+  vTaskStartScheduler();
+  // MUST NOT reach this while
+  while(1);
+}
+
+
+
+
+
+
+void mainTask(void * arg) {
+
+  MX_USB_DEVICE_Init();
+
+  std::string input = (char *) userRxData;
+  while(1) {
 
     if(strlen((char *) userRxData) > 0) {
+
+
+
+
       strcpy(txData, "echo: ");
       strcat(txData, (char *) userRxData);
       strcat(txData, (char *) '\n');
-  //    CDC_Transmit_FS((uint8_t*) txData, strlen(txData));
       CDC_Transmit_FS((uint8_t*) txData, strlen(txData));
       memset(txData, '\0', 64);
       memset(userRxData, '\0', 64);
     }
-
-    HAL_Delay(1);
-
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
+
 }
